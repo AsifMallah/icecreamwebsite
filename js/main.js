f1725ae7ea86b70476bdb6dd64d2748ee1ced37c
AsifMallah <libra.mallah@gmail.com>
2025-06-19 00:58:43 +0500

    Update project files

--- js/main.js
+++ js/main.js
@@ -133,3 +133,117 @@
     
 })(jQuery);
 
+
+
+  function switchToSection(id) {
+    document.querySelectorAll('.product-section').forEach(section => section.classList.add('d-none'));
+    document.getElementById(id).classList.remove('d-none');
+  }
+
+  const popFlavors = [
+    { name: 'Wild Blueberry', img: 'img/WB.png' },
+    { name: 'Desi Imlee Aloo Bukhara', img: 'img/DI.png' },
+    { name: 'Juicy Pineapple', img: 'img/JP.png' },
+    { name: 'Energetic Strawberry', img: 'img/ES.png' },
+    { name: 'Cola Bomb', img: 'img/CB.png' },
+    { name: 'Lychee Fruity', img: 'img/LF.png' }
+  ];
+  let popIndex = 0;
+  function changePopFlavor(d) {
+    popIndex = (popIndex + d + popFlavors.length) % popFlavors.length;
+    document.getElementById("pop-img").src = popFlavors[popIndex].img;
+    document.getElementById("pop-name").textContent = popFlavors[popIndex].name;
+    document.getElementById("pop-list").textContent = popFlavors[popIndex].name;
+  }
+
+  const cupFlavors = [
+    { name: "Mango", img: "img/MA.png" },
+    { name: "Strawberry", img: "img/SB.png" },
+    { name: "Vanilla", img: "img/VA.png" },
+    { name: "Kulfa", img: "img/KU.png" },
+    { name: "Pista", img: "img/PI.png" }
+  ];
+  let cupIndex = 0;
+  function changeCupFlavor(d) {
+    cupIndex = (cupIndex + d + cupFlavors.length) % cupFlavors.length;
+    document.getElementById("cup-img").src = cupFlavors[cupIndex].img;
+    document.getElementById("cup-name").textContent = cupFlavors[cupIndex].name;
+    document.getElementById("cup-list").textContent = cupFlavors[cupIndex].name;
+  }
+
+  const popsicleFlavors = [
+    { name: 'Mango Bite', img: 'img/popM.jpeg' },
+    { name: 'Orange Pop', img: 'img/popO.jpeg' },
+    { name: 'Falsa', img: 'img/popF.jpeg' }
+  ];
+  let popsicleIndex = 0;
+  function changePopsicleFlavor(d) {
+    popsicleIndex = (popsicleIndex + d + popsicleFlavors.length) % popsicleFlavors.length;
+    document.getElementById("popsicle-img").src = popsicleFlavors[popsicleIndex].img;
+    document.getElementById("popsicle-name").textContent = popsicleFlavors[popsicleIndex].name;
+    document.getElementById("popsicle-list").textContent = popsicleFlavors[popsicleIndex].name;
+  }
+
+
+  const chocbarFlavors = [
+    { name: 'Nutty Chocolate', img: 'img/choc1.jpeg', price: 80 },
+    { name: 'Choc Bar', img: 'img/choc2.jpeg', price: 50 },
+    { name: 'Choco Dip', img: 'img/choc3.jpeg', price: 40 }
+  ];
+
+  let chocbarIndex = 0;
+
+  function changeChocbarFlavor(d) {
+    chocbarIndex = (chocbarIndex + d + chocbarFlavors.length) % chocbarFlavors.length;
+    const flavor = chocbarFlavors[chocbarIndex];
+    document.getElementById("chocbar-img").src = flavor.img;
+    document.getElementById("chocbar-name").textContent = flavor.name;
+    document.getElementById("chocbar-list").textContent = flavor.name;
+    document.getElementById("chocbar-price").textContent = `Rs ${flavor.price}`;
+  }
+
+
+  const kulfaFlavors = [
+    { name: 'Crunchy Badami', img: 'img/BKK.png' },
+    { name: 'Pista Delight', img: 'img/pkk.png' }
+  ];
+  let kulfaIndex = 0;
+  function changeKulfaFlavor(d) {
+    kulfaIndex = (kulfaIndex + d + kulfaFlavors.length) % kulfaFlavors.length;
+    document.getElementById("kulfa-img").src = kulfaFlavors[kulfaIndex].img;
+    document.getElementById("kulfa-name").textContent = kulfaFlavors[kulfaIndex].name;
+    document.getElementById("kulfa-list").textContent = kulfaFlavors[kulfaIndex].name;
+  }
+
+  const brickFlavors = [
+  { name: 'Strawberry', img: 'img/SBB.png' },
+    { name: 'Kulfa', img: 'img/KBB.png' },
+    { name: 'Chocolate', img: 'img/CBB.png' },
+    { name: 'Pista', img: 'img/PBB.png' },
+    { name: 'Mango', img: 'img/MBB.png' }
+  ];
+  let brickIndex = 0;
+  function changeBrickFlavor(d) {
+    brickIndex = (brickIndex + d + brickFlavors.length) % brickFlavors.length;
+    document.getElementById("brick-img").src = brickFlavors[brickIndex].img;
+    document.getElementById("brick-name").textContent = brickFlavors[brickIndex].name;
+    document.getElementById("brick-list").textContent = brickFlavors[brickIndex].name;
+  }
+
+  const coneFlavors = [  
+    { name: 'Chocolate', img: 'img/CCC.png' },
+    { name: 'Strawberry', img: 'img/SCC.png' },
+    ];
+  let coneIndex = 0;
+  function changeConeFlavor(d) {
+    coneIndex = (coneIndex + d + coneFlavors.length) % coneFlavors.length;
+    document.getElementById("cone-img").src = coneFlavors[coneIndex].img;
+    document.getElementById("cone-name").textContent = coneFlavors[coneIndex].name;
+    document.getElementById("cone-list").textContent = coneFlavors[coneIndex].name;
+  }
+
+    function orderNow() {
+    const phoneNumber = "+923112811272"; // Replace with your WhatsApp number
+  const message = encodeURIComponent("Hello, I would like to order some ice cream.");
+window.open(`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`, '_blank');}
+
